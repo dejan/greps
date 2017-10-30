bedc6c24f09d6283d91aedc21384bb0e4228c76b
Dejan Simic <desimic@gmail.com>
2017-10-30 13:37:51 +0100

    Fixes the silent exit on encountering a long line

--- cmd/greps/main.go
+++ cmd/greps/main.go
@@ -14,6 +14,8 @@ const (
 	reset          = "\033[0m"
 	infoColor      = fgCyan
 	highlightColor = bgRed
+	maxLineSize    = 1024 * 1024
+	startBufSize   = 1024 * 16
 )
 
 func main() {
@@ -28,6 +30,7 @@ func main() {
 		os.Exit(0)
 	}
 	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Buffer(make([]byte, startBufSize), maxLineSize)
 	var counter int64
 	for scanner.Scan() {
 		text := scanner.Text()
@@ -38,6 +41,9 @@ func main() {
 			counter++
 			fmt.Print(infoColor, "\r", counter, " lines skipped", reset)
 		}
+	}
+	if scanner.Err() != nil {
+		fmt.Fprintf(os.Stderr, "\nError occurred: %v\n", scanner.Err())
 	}
 	fmt.Println()
 }
